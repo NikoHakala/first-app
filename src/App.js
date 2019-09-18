e4e338ed15a676b2cb43b9a00c433292753df8db
NikoHakala <Niko_hakala_94@hotmail.com>
2019-09-18 11:09:02 +0300

    Working app.js

--- src/App.js
+++ src/App.js
@@ -2,36 +2,37 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [todo, setTodo] = useState('');
-  const [date, setDate] = useState('');
+  const [todos, setTodos] = useState({desc: '', date: ''});
+
   const [listItems, setListItems] = useState([]);
 
   const inputChanged = (event) => {
-    setTodo(event.target.value);
-    setDate(event.target.value);
+    setTodos({...todos, [event.target.name]: event.target.value});
   }
 
   const addTodo = (event) => {
     event.preventDefault();
-    setListItems([listItems, todo]);
+    setListItems([...listItems, todos]);
   }
 
   return (
     <div className="App">
       <form onSubmit={addTodo}>
         Description:
-        <input type="text" onChange={inputChanged} value={todo}/>
+        <input type="text" onChange={inputChanged} name="desc" value={todos.desc}/>
         Date:
-        <input type="text" onChange={inputChanged} value={date}/>
+        <input type="text" onChange={inputChanged} name="date" value={todos.date}/>
         <input type="Submit" value="add"/>
+        
+        <table><tbody>
+          {listItems.map((item, index) =>
+            <tr key={index}>
+              <td>{item.desc}</td>
+              <td>{item.date}</td>
+            </tr>)
+          }
+        </tbody></table>
       </form>
-      <table><tbody>
-        {listItems.map((item, index) =>
-          <tr key={index}>
-            <td>{item}</td>
-          </tr>)
-        }
-      </tbody></table>
     </div>
   );
 }
